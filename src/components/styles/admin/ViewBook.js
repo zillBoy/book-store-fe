95349d9e8423c82f777a01cdc7741bb82f2b19f8
Muhammad Usman <prousman741@gmail.com>
2021-10-02 18:48:20 +0500

    ViewBooks - (inital setup implemented) getting data from API

---
+++ src/components/styles/admin/ViewBook.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const TableContainer = styled.div`
+    display: block;
+    position: relative;
+    top: 60px;
+    margin-left: 1%;
+    width: 95%;
+    height: 90vh;
+    background-color: #fff;
+    border-radius: 5px;
+    /* overflow-y: scroll; */
+`;
+
+export const Table = styled.table`
+    width: 100%;
+    border-collapse: collapse;
+`;
+
+export const TableHead = styled.thead`
+
+`;
+
+export const TableHeader = styled.th`
+    text-align: left;
+    background-color: #B28E85;
+    padding: 10px;
+`;
+
+export const TableBody = styled.tbody`
+    
+`;
+
+export const TableRow = styled.tr`
+`;
+
+export const TableData = styled.td`
+    padding: 10px;
+    border-bottom: 1px solid #CCCCCC;
+`;
