28ec1083a58418e0db124e188e234a60811790df
Muhammad Usman <prousman741@gmail.com>
2021-10-02 18:48:13 +0500

    ViewBooks - (inital setup implemented) getting data from API

--- src/components/pages/admin/ViewBooks.js
+++ src/components/pages/admin/ViewBooks.js
@@ -1,10 +1,48 @@
 import React from 'react'
+import { TableContainer, Table, TableRow, TableHead, TableHeader, TableData, TableBody } from '../../styles/admin/ViewBook'
+import Layout from '../../Layout'
+import useBooks from '../../../hooks/useBooks'
 
 const ViewBooks = () => {
+
+    const books = useBooks()
+    console.log('books: ', books)
+
     return (
-        <div>
-            <p>This is the view books component can be visible only to admin</p>
-        </div>
+        <Layout>
+            <TableContainer>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableHeader>Name</TableHeader>
+                            <TableHeader>Author</TableHeader>
+                            <TableHeader>Publisher</TableHeader>
+                            <TableHeader>ISBN</TableHeader>
+                            <TableHeader>Year</TableHeader>
+                            <TableHeader>Page</TableHeader>
+                            <TableHeader>Genre</TableHeader>
+                            <TableHeader>Language</TableHeader>
+                            <TableHeader>Actions</TableHeader>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {books.map(book => (
+                            <TableRow>
+                                <TableData>{book.name}</TableData>
+                                <TableData>{book.author}</TableData>
+                                <TableData>{book.publisher}</TableData>
+                                <TableData>{book.isbn}</TableData>
+                                <TableData>{book.year}</TableData>
+                                <TableData>{book.page}</TableData>
+                                <TableData>{book.genre}</TableData>
+                                <TableData>{book.language}</TableData>
+                                <TableData>a</TableData>
+                            </TableRow>
+                        ))}
+                    </TableBody>        
+                </Table>
+            </TableContainer>
+        </Layout>
     )
 }
 
